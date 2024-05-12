843c4f362b6a3c5e7d25ba8ef26dbe40dc2315a4
Davi Pereira <davi.p.m94@gmail.com>
2024-05-12 23:03:22 +0900

    update app

---
+++ components/cursor/live-cursors.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export function LiveCursors() {
+  const [item, setItem] = useState(null);
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}

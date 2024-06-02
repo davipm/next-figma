76e04313ecf1fed903a96d7fa51182825565515c
Davi Pereira <davi.p.m94@gmail.com>
2024-06-02 17:12:43 +0900

    adding color

--- components/settings/color.tsx
+++ components/settings/color.tsx
@@ -8,7 +8,13 @@ type Props = {
   handleInputChange: (property: string, value: string) => void;
 };
 
-export function Color({ placeholder, inputRef, attribute }: Props) {
+export function Color({
+  placeholder,
+  inputRef,
+  attribute,
+  handleInputChange,
+  attributeType,
+}: Props) {
   return (
     <div className="flex flex-col gap-3 border-b border-primary-grey-200 p-5">
       <h3 className="text-[10px] uppercase">{placeholder}</h3>
@@ -16,9 +22,18 @@ export function Color({ placeholder, inputRef, attribute }: Props) {
         className="flex items-center gap-2 border border-primary-grey-200"
         onClick={() => inputRef.current.click()}
       >
-        <input type="text" />
-        <label className="flex-1">{attribute}</label>
-        <label className="">90%</label>
+        <input
+          type="color"
+          value={attribute}
+          ref={inputRef}
+          onChange={(event) =>
+            handleInputChange(attributeType, event.target.value)
+          }
+        />
+        <Label className="flex-1">{attribute}</Label>
+        <Label className="flex h-6 w-8 items-center justify-center bg-primary-grey-100 text-[10px] leading-3">
+          90%
+        </Label>
       </div>
     </div>
   );

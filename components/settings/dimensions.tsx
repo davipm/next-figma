7605dd34a33ea6f8a8835fe25ac76704cdfe8a47
Davi Pereira <davi.p.m94@gmail.com>
2024-06-02 17:15:35 +0900

    add dimensions.tsx

--- components/settings/dimensions.tsx
+++ components/settings/dimensions.tsx
@@ -1,15 +1,53 @@
-"use client";
+import { MutableRefObject } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
-import { useState } from "react";
+const dimensionsOptions = [
+  { label: "W", property: "width" },
+  { label: "H", property: "height" },
+];
 
-// import { Container } from "./styles";
+type Props = {
+  width: string;
+  height: string;
+  isEditingRef: MutableRefObject<boolean>;
+  handleInputChange: (property: string, value: string) => void;
+};
 
-export function Dimensions() {
-  const [item, setItem] = useState(null);
-
+export function Dimensions({
+  width,
+  height,
+  isEditingRef,
+  handleInputChange,
+}: Props) {
   return (
-    <div>
-      <p></p>
-    </div>
+    <section className="flex flex-col border-b border-primary-grey-200">
+      <div className="flex flex-col gap-4 px-6 py-3">
+        {dimensionsOptions.map((item) => (
+          <div
+            key={item.label}
+            className="flex flex-1 items-center gap-3 rounded-sm"
+          >
+            <label htmlFor={item.property} className="text-[10px] font-bold">
+              {item.label}
+            </label>
+            <input
+              type="number"
+              id={item.property}
+              placeholder="100"
+              value={item.property === "width" ? width : height}
+              className="input-ring"
+              min={10}
+              onChange={(event) =>
+                handleInputChange(item.property, event.target.value)
+              }
+              onBlur={() => {
+                isEditingRef.current = false;
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }

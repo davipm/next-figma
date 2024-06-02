e44a5801fa73c8b667119fab9e932aae71d988ff
Davi Pereira <davi.p.m94@gmail.com>
2024-06-02 17:44:02 +0900

    adding text component

--- components/settings/text.tsx
+++ components/settings/text.tsx
@@ -1,15 +1,86 @@
-"use client";
+import {
+  fontFamilyOptions,
+  fontSizeOptions,
+  fontWeightOptions,
+} from "@/constants";
 
-import { useState } from "react";
+import { Select } from "@/components/ui/select";
 
-// import { Container } from "./styles";
+const selectConfigs = [
+  {
+    property: "fontFamily",
+    placeholder: "Choose a font",
+    options: fontFamilyOptions,
+  },
+  { property: "fontSize", placeholder: "30", options: fontSizeOptions },
+  {
+    property: "fontWeight",
+    placeholder: "SemiBold",
+    options: fontWeightOptions,
+  },
+];
 
-export function Text() {
-  const [item, setItem] = useState(null);
+type TextProps = {
+  fontFamily: string;
+  fontSize: string;
+  fontWeight: string;
+  handleInputChange: (property: string, value: string) => void;
+};
 
+export function Text({
+  fontFamily,
+  fontSize,
+  fontWeight,
+  handleInputChange,
+}: TextProps) {
   return (
-    <div>
-      <p></p>
+    <div className="flex flex-col gap-3 border-b border-primary-grey-200 px-5 py-3">
+      <h3 className="text-[10px] uppercase">Text</h3>
+      <div className="flex flex-col gap-3">
+        {RenderSelect({
+          config: selectConfigs[0],
+          fontSize,
+          fontWeight,
+          fontFamily,
+          handleInputChange,
+        })}
+
+        <div className="flex gap-2">
+          {selectConfigs.slice(1).map((config) =>
+            RenderSelect({
+              config,
+              fontSize,
+              fontWeight,
+              fontFamily,
+              handleInputChange,
+            }),
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
+type Props = {
+  config: {
+    property: string;
+    placeholder: string;
+    options: { label: string; value: string }[];
+  };
+  fontSize: string;
+  fontWeight: string;
+  fontFamily: string;
+  handleInputChange: (property: string, value: string) => void;
+};
+
+const RenderSelect = ({
+  config,
+  fontSize,
+  fontWeight,
+  fontFamily,
+  handleInputChange,
+}: Props) => (
+  <div>
+    <div></div>
+  </div>
+);

9ec3590d6176675dfb235d318cb41098aed33e65
Davi Pereira <davi.p.m94@gmail.com>
2024-06-02 17:15:53 +0900

    add export component

--- components/settings/export.tsx
+++ components/settings/export.tsx
@@ -1,15 +1,16 @@
-"use client";
-
-import { useState } from "react";
-
-// import { Container } from "./styles";
+import { exportToPdf } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export function Export() {
-  const [item, setItem] = useState(null);
-
   return (
-    <div>
-      <p></p>
+    <div className="flex flex-col gap-3 px-5 py-3">
+      <h3 className="text-[10px] uppercase">Export</h3>
+      <Button
+        onClick={exportToPdf}
+        className="w-full border border-primary-grey-100 hover:bg-primary-green hover:text-primary-black"
+      >
+        Export to PDF
+      </Button>
     </div>
   );
 }
